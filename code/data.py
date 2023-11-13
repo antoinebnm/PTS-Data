b53561ae53940211178443e735ab8170852213fe
Antoine Bonhomme <ab20040218@gmail.com>
2023-11-13 15:26:25 +0100

    save

--- code/data.py
+++ code/data.py
@@ -5,12 +5,14 @@ import pandas as pd
 import seaborn as sns
 from scipy import stats
 
-sets = [f for f in os.listdir("datasets")]
-data = {}
 
-for set in sets:
-    data[set.removeprefix("interventions").removesuffix(".csv")] = pd.read_csv("datasets" + os.sep + set, sep=";", index_col=0, encoding='latin-1')
-
-INDEX = str(input("Année (de 2008 à 2021) : "))
-
-print(data[INDEX].head())
+def extract_data(year=2008):
+    sets = [f for f in os.listdir("datasets")]
+    data = {}
+    
+    for set in sets:
+        data[int(set.removeprefix("interventions").removesuffix(".csv"))] = pd.read_csv("datasets" + os.sep + set, sep=";", index_col=0, encoding='latin-1')
+    
+#    INDEX = str(input("Année (de 2008 à 2021) : "))
+#    print(data[INDEX].head())
+    return data[year].head()
