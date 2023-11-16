94ad688896439388d26f67ede3a1e8e7c5998c95
Antoine Bonhomme <ab20040218@gmail.com>
2023-11-16 21:07:52 +0100

    data.py updated : create datasets

--- code/data.py
+++ code/data.py
@@ -6,13 +6,28 @@ import seaborn as sns
 from scipy import stats
 
 
-def extract_data(year=2008):
+def extract_data(year="2008"):
+    year = int(year)
     sets = [f for f in os.listdir("datasets")]
     data = {}
     
     for set in sets:
         data[int(set.removeprefix("interventions").removesuffix(".csv"))] = pd.read_csv("datasets" + os.sep + set, sep=";", index_col=0, encoding='latin-1')
-    
-#    INDEX = str(input("Année (de 2008 à 2021) : "))
-#    print(data[INDEX].head())
+
     return data[year].head()
+
+def create_dataset(category="Dataset Column", years=["a","b","..."]):
+    new_dataset = {}
+    for year in years:
+        data = extract_data(year)
+
+    new_my_dict = [
+    {'a': 15, 'n': 81, 'p': 177}
+    ]
+    df = pd.DataFrame.from_dict(new_my_dict) 
+    df.to_csv (r'test8.csv', index=False, header=True)
+
+
+if __name__ == "__main__":
+    dataset = extract_data(input())
+
